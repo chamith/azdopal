91e53828d2604c018ccdafc8cfc8dd3abbaa1b89
Chamith Siriwardena <chamith@gmail.com>
2026-04-28 15:48:37 +0530

    adding files

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 trap 'kill 0' EXIT
 
 echo "Starting API server on http://localhost:8000 ..."
-uvicorn web.api.main:app --reload --port 8000 &
+python3 -m uvicorn web.api.main:app --reload --port 8000 &
 
 echo "Starting UI server on http://localhost:5173 ..."
 cd web/ui && npm run dev &
